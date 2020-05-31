6736efe06ed2099c7fa34ffde5fcfecf4af20fa8
Magnesium- <757057999@qq.com>
2020-06-01 02:27:03 +0800

    lint-01

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   presets: [
-    "@babel/preset-react"
-  ]
-}
+    '@babel/preset-react',
+  ],
+};
--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
-// import './App-css.css';
 import './App-scss.scss';
 
-import img from './assets/images/logo.svg'
+import img from './assets/images/logo.svg';
 
-export function App() {
+export default function App() {
   return (
     <div className="app">
-      <img className="app-logo" src={img} alt="logo"/>
+      <img className="app-logo" src={img} alt="logo" />
       <h1>中后台管理系统</h1>
     </div>
-  )
+  );
 }
